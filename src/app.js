d8f91b9d8317bd9f537870771bfb5935b45f2686
Felipe Caranton <piperscarantonnn@hotmail-com>
2026-04-22 20:52:23 -0500

    fix cors with manual headers

--- src/app.js
+++ src/app.js
@@ -6,13 +6,18 @@
 const logger = require('./middleware/logger')
 const error = require('./middleware/errorHandler')
 const express = require('express');
-const cors = require('cors');
 const routes  = require('./routes')
 require('dotenv').config();
 const app = express();
 
 
-app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') return res.sendStatus(200);
+    next();
+});
 app.use(express.json()); //It parses incoming requests with JSON payloads and makes the data available in:
 app.use(logger);
 
