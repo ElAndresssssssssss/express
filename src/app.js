2511ef54383a404a8ee73aa53e59a99b7c2de115
Felipe Caranton <piperscarantonnn@hotmail-com>
2026-04-22 21:49:33 -0500

    remove docs folder

--- src/app.js
+++ src/app.js
@@ -6,27 +6,17 @@
 const logger = require('./middleware/logger')
 const error = require('./middleware/errorHandler')
 const express = require('express');
-const path = require('path');
 const routes  = require('./routes')
 require('dotenv').config();
 const app = express();
-const frontendPath = path.join(__dirname, '..', 'frontend');
 
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') return res.sendStatus(200);
-    next();
-});
 app.use(express.json()); //It parses incoming requests with JSON payloads and makes the data available in:
 app.use(logger);
 
-app.use('/api', routes);
-app.use(express.static(frontendPath));
+app.use('/api', routes); //it says that this one is going to be the main point
 app.get('/', (req, res) => {
-    res.sendFile(path.join(frontendPath, 'index.html'));
+    res.send('Hello world')
 });
 app.use(error);
 
